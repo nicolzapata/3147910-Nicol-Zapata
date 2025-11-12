d87e82a71a446005fdc714c01e2ff73901717046
nicolzapata <zapatanicol094@gmail.com>
2025-11-12 14:10:34 -0500

    Eliminar Crudnativo - proyectox

--- Taller-proyectoMVC_NicolZapata/Taller-proyectoMVC_NicolZapata/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Taller_proyectoMVC_NicolZapata.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
